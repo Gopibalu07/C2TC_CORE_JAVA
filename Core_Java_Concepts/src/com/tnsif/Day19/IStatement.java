774f26f64e83ceb94b416d5743b08fac7d2c98f8
Gopinath <gopinathbalu07@gmail.com>
2025-10-26 08:07:05 +0530

    Committed initially

---
+++ Core_Java_Concepts/src/com/tnsif/Day19/IStatement.java
@@ -0,0 +1,5 @@
+package com.tnsif.Day19;
+
+public interface IStatement {
+	public String show();// abstract Method
+}
---
+++ Core_Java_Concepts/src/com/tnsif/Day19/IsOdd.java
@@ -0,0 +1,5 @@
+package com.tnsif.Day19;
+
+public interface IsOdd {
+	public boolean CheckOdd(int a);
+}
